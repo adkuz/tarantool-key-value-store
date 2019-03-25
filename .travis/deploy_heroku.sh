53cb738470268ac77b1c966a4019805b5381d09c
ALexander <ad.kuznetsov.a7@Yandex.ru>
2019-03-25 03:26:10 +0300

    -

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
+heroku plugins:install @heroku-cli/plugin-container-registry
 
-docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+echo $HEROKU_API_KEY | docker login -u _  --password-stdin registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
