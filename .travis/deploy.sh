9f79c68e098ae01795b14821d7d60f625dc28665
ALexander <ad.kuznetsov.a7@Yandex.ru>
2019-03-25 03:21:49 +0300

    -

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
 
-
 echo "$DOCKER_PASS" | docker login -u $DOCKER_USER --password-stdin
 docker build -f Dockerfile -t $(echo $TRAVIS_REPO_SLUG:$TAG | awk '{print tolower($0)}') .
-docker push $(echo $TRAVIS_REPO_SLUG | awk '{print tolower($0)}')
+docker push $(echo $TRAVIS_REPO_SLUG:$TAG | awk '{print tolower($0)}')
