fcaaa5ead7e1bd5c1ad551dd94cf3032d780cba1
ALexander <ad.kuznetsov.a7@Yandex.ru>
2019-04-05 00:43:38 +0300

    feature: push docker to hub

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,6 +7,7 @@ else
 fi
 
 echo "$DOCKER_PASS" | docker login -u $DOCKER_USER --password-stdin
-export DOCKER_IMAGE=$(echo $TRAVIS_REPO_SLUG:$TAG | awk '{print tolower($0)}')
-docker build -f Dockerfile -t $DOCKER_IMAGE .
-docker push $DOCKER_IMAGE
+# export DOCKER_IMAGE=$(echo $TRAVIS_REPO_SLUG:$TAG | awk '{print tolower($0)}')
+export DOCKER_IMAGE=ax_tarantool:$TAG
+docker build -f Dockerfile -t $DOCKER_USER/$DOCKER_IMAGE .
+docker push $DOCKER_USER/$DOCKER_IMAGE
