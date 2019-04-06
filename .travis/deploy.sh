f3241cab344fc72b10310cd1d8ffa8f0f4790829
ALexander <ad.kuznetsov.a7@Yandex.ru>
2019-04-07 00:59:39 +0300

    another push

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
@@ -7,7 +8,11 @@ else
 fi
 
 echo "$DOCKER_PASS" | docker login -u $DOCKER_USER --password-stdin
-# export DOCKER_IMAGE=$(echo $TRAVIS_REPO_SLUG:$TAG | awk '{print tolower($0)}')
+
 export DOCKER_IMAGE=ax_tarantool:$TAG
-docker build -f Dockerfile -t $DOCKER_USER/$DOCKER_IMAGE .
-docker push $DOCKER_USER/$DOCKER_IMAGE
+
+# docker build -f Dockerfile -t $DOCKER_USER/$DOCKER_IMAGE .
+docker build --tag=registry.heroku.com/${HEROKU_APP_NAME}/web .
+
+# docker push $DOCKER_USER/$DOCKER_IMAGE
+docker push registry.heroku.com/${HEROKU_APP_NAME}/web
